c49b02a6333e02ce92dba7a16178cc78025cd90a
Alex <almartin86@yahoo.fr>
2020-03-03 13:51:09 +0100

    maj arbo + bdd en groupe

--- 01-bddSql/gestion_stock.sql
+++ 01-bddSql/gestion_stock.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 02 mars 2020 à 13:05
--- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Client :  localhost
+-- Généré le :  Mar 03 Mars 2020 à 12:45
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `gestion_stock`
 --
+CREATE DATABASE IF NOT EXISTS `gestion_stock` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `gestion_stock`;
 
 -- --------------------------------------------------------
 
@@ -28,13 +28,11 @@ SET time_zone = "+00:00";
 -- Structure de la table `logs_connexions`
 --
 
-DROP TABLE IF EXISTS `logs_connexions`;
-CREATE TABLE IF NOT EXISTS `logs_connexions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `logs_connexions` (
+  `id` int(11) NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
-  `date_connexion` date NOT NULL,
-  `connecte` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `date_connexion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `connecte` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -43,18 +41,16 @@ CREATE TABLE IF NOT EXISTS `logs_connexions` (
 -- Structure de la table `produits`
 --
 
-DROP TABLE IF EXISTS `produits`;
-CREATE TABLE IF NOT EXISTS `produits` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `produits` (
+  `id` int(11) NOT NULL,
   `code_produit` varchar(16) NOT NULL,
-  `nom` varchar(255) NOT NULL,
+  `nom` varchar(64) NOT NULL,
   `detail` text NOT NULL,
-  `categorie` varchar(255) NOT NULL,
+  `categorie` varchar(64) NOT NULL,
   `quantite` int(11) NOT NULL,
   `prix_achat` float NOT NULL,
-  `date_expiration` date NOT NULL,
-  `actif` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `date_expiration` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `actif` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -63,18 +59,69 @@ CREATE TABLE IF NOT EXISTS `produits` (
 -- Structure de la table `utilisateurs`
 --
 
-DROP TABLE IF EXISTS `utilisateurs`;
-CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `mot_de_passe` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `actif` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `type` varchar(64) NOT NULL,
+  `actif` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-COMMIT;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `logs_connexions`
+--
+ALTER TABLE `logs_connexions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_utilisateur` (`id_utilisateur`);
+
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `logs_connexions`
+--
+ALTER TABLE `logs_connexions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `logs_connexions`
+--
+ALTER TABLE `logs_connexions`
+  ADD CONSTRAINT `logs_connexions_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
