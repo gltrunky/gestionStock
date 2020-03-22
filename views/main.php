f8f2a47557625d76ae951e336fbc731dd981a276
Alex <almartin86@yahoo.fr>
2020-03-22 19:43:31 +0100

    Mise en place du dashboard

--- views/main.php
+++ views/main.php
@@ -1,10 +1,103 @@
-<div class="container-fluid dashcorp">
+<div class="dashcorp">
     <div class="row">
-        <div class="col col-lg-2 dashboard">
-            Menu
+        <div class="bg-secondary col-sm-5 col-md-3 col-lg-2 col-xl-2">
+            <!--Tableau de bord-->
+            <div class="row border-danger border-bottom pl-1">
+                <div class="col-4 bg-light mt-2 ml-2 mb-2 text-center border border-dark">
+                    <i class="fas fa-user  p-4"></i>
+                </div>
+                <div class="col-7 pt-4">
+                    <h6><strong>Admin</strong></br> Profil <i class="fas fa-unlock-alt"></i> Déconnexion</h6>
+                </div>
+            </div>
+            <nav class="mr-auto ml-auto">
+                <h4 class="mt-2 border-light border-bottom text-center"><i class="fas fa-desktop"></i> TABLEAU DE BORD</h4>
+                <div class="row ml-auto mr-auto p-1">
+                    <a class="btn btn-success col-5 m-2" href="#"><i class="fas fa-shopping-cart"></i></br>Entrée</a>
+                    <a class="btn btn-danger col-5 m-2" href="#"><i class="fas fa-shopping-cart"></i></br>Sortie</a>
+                </div>
+                <div class="row ml-auto mr-auto p-1">
+                    <a class="btn btn-warning col-5 m-2" href="#"><i class="fas fa-user"></i></br>Utilisateurs</a>
+                    <a class="btn btn-info col-5 m-2" href="#"><i class="fas fa-chart-line"></i></br>Statistiques</a>
+                </div>
+                <div class="row ml-auto mr-auto p-1">
+                    <a class="btn btn-danger col-5 m-2" href="#"><i class="fas fa-box-open"></i></br>Stock</a>
+                    <a class="btn btn-success col-5 m-2" href="#"><i class="fas fa-book"></i></br>Journal</a>
+                </div>
+                <div class="row ml-auto mr-auto p-1">
+                    <a class="btn btn-info col-5 m-2" href="#"><i class="fas fa-user"></i></br>Client</a>
+                    <a class="btn btn-warning col-5 m-2" href="#"><i class="fas fa-network-wired"></i></br>Catégorie</a>
+                </div>
+            </nav>
         </div>
-        <div class="col col-lg-10 corp">
-            Affichage stock
+        <div class="bg-light col-sm-7 col-md-9 col-lg-10 col-xl-10">
+            <!--CORP-->
+            <table class="table table-hover table-secondary container-fluid">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Prenom</th>
+                        <th scope="col">Mail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Martin</td>
+                        <td>Alexandre</td>
+                        <td>alexandremartin@gmail.fr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Murashka</td>
+                        <td>Olga</td>
+                        <td>murashka.olga@gmail.fr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ostalier</td>
+                        <td>Laurent</td>
+                        <td>ostalier.laurent@gmail.fr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ostalier</td>
+                        <td>Laurent</td>
+                        <td>ostalier.laurent@gmail.fr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ostalier</td>
+                        <td>Laurent</td>
+                        <td>ostalier.laurent@gmail.fr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ostalier</td>
+                        <td>Laurent</td>
+                        <td>ostalier.laurent@gmail.fr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ostalier</td>
+                        <td>Laurent</td>
+                        <td>ostalier.laurent@gmail.fr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ostalier</td>
+                        <td>Laurent</td>
+                        <td>ostalier.laurent@gmail.fr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ostalier</td>
+                        <td>Laurent</td>
+                        <td>ostalier.laurent@gmail.fr</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
