82cef2842a2d7e20a1f70c8fbea0369b3ec7d9f5
Alex <almartin86@yahoo.fr>
2020-03-22 23:11:10 +0100

    maj dashboard

--- views/main.php
+++ views/main.php
@@ -1,36 +1,38 @@
 <div class="dashcorp">
     <div class="row">
-        <div class="bg-secondary col-sm-5 col-md-3 col-lg-2 col-xl-2">
+        <div class="bg-secondary col-sm-4 col-md-4 col-lg-3 col-xl-3">
             <!--Tableau de bord-->
-            <div class="row border-danger border-bottom pl-1">
-                <div class="col-4 bg-light mt-2 ml-2 mb-2 text-center border border-dark">
-                    <i class="fas fa-user  p-4"></i>
-                </div>
-                <div class="col-7 pt-4">
-                    <h6><strong>Admin</strong></br> Profil <i class="fas fa-unlock-alt"></i> Déconnexion</h6>
+            <div class="container-fluid border-danger border-bottom pl-1 ml-1">
+                <div class="row">
+                    <div class="col-4 bg-light mt-2 ml-2 mb-2 text-center border border-dark">
+                        <i class="fas fa-user p-3 useri"></i>
+                    </div>
+                    <div class="col-7 pt-4">
+                        <h6><strong>Admin</strong></br> Profil <i class="fas fa-unlock-alt"></i> Déconnexion</h6>
+                    </div>
                 </div>
             </div>
-            <nav class="mr-auto ml-auto">
+            <nav class="container-fluid">
                 <h4 class="mt-2 border-light border-bottom text-center"><i class="fas fa-desktop"></i> TABLEAU DE BORD</h4>
-                <div class="row ml-auto mr-auto p-1">
-                    <a class="btn btn-success col-5 m-2" href="#"><i class="fas fa-shopping-cart"></i></br>Entrée</a>
-                    <a class="btn btn-danger col-5 m-2" href="#"><i class="fas fa-shopping-cart"></i></br>Sortie</a>
+                <div class="row ml-auto mr-auto">
+                    <button class="btn btn-success btn-sm col-5 mt-2 mb-2 ml-auto mr-auto p-2"><i class="fas fa-shopping-cart"></i></br>Entrée</button>
+                    <button class="btn btn-danger btn-sm col-5 mt-2 mb-2 ml-auto mr-auto p-2" href="#"><i class="fas fa-shopping-cart"></i></br>Sortie</button>
                 </div>
-                <div class="row ml-auto mr-auto p-1">
-                    <a class="btn btn-warning col-5 m-2" href="#"><i class="fas fa-user"></i></br>Utilisateurs</a>
-                    <a class="btn btn-info col-5 m-2" href="#"><i class="fas fa-chart-line"></i></br>Statistiques</a>
+                <div class="row ml-auto mr-auto">
+                    <button class="btn btn-warning btn-sm  col-5 mt-2 mb-2 ml-auto mr-auto p-2" href="#"><i class="fas fa-user"></i></br>Utilisateurs</button>
+                    <button class="btn btn-info btn-sm  col-5 mt-2 mb-2 ml-auto mr-auto p-2" href="#"><i class="fas fa-chart-line"></i></br>Statistiques</button>
                 </div>
-                <div class="row ml-auto mr-auto p-1">
-                    <a class="btn btn-danger col-5 m-2" href="#"><i class="fas fa-box-open"></i></br>Stock</a>
-                    <a class="btn btn-success col-5 m-2" href="#"><i class="fas fa-book"></i></br>Journal</a>
+                <div class="row ml-auto mr-auto">
+                    <button class="btn btn-danger btn-sm col-5 mt-2 mb-2 ml-auto mr-auto p-2" href="#"><i class="fas fa-box-open"></i></br>Stock</button>
+                    <button class="btn btn-success btn-sm col-5 mt-2 mb-2 ml-auto mr-auto p-2" href="#"><i class="fas fa-book"></i></br>Journal</button>
                 </div>
-                <div class="row ml-auto mr-auto p-1">
-                    <a class="btn btn-info col-5 m-2" href="#"><i class="fas fa-user"></i></br>Client</a>
-                    <a class="btn btn-warning col-5 m-2" href="#"><i class="fas fa-network-wired"></i></br>Catégorie</a>
+                <div class="row ml-auto mr-auto">
+                    <button class="btn btn-info btn-sm col-5 mt-2 mb-2 ml-auto mr-auto p-2" href="#"><i class="fas fa-user"></i></br>Client</button>
+                    <button class="btn btn-warning btn-sm col-5 mt-2 mb-2 ml-auto mr-auto p-2" href="#"><i class="fas fa-network-wired"></i></br>Catégorie</button>
                 </div>
             </nav>
         </div>
-        <div class="bg-light col-sm-7 col-md-9 col-lg-10 col-xl-10">
+        <div class="bg-light col-sm-8 col-md-8 col-lg-9 col-xl-9">
             <!--CORP-->
             <table class="table table-hover table-secondary container-fluid">
                 <thead>
