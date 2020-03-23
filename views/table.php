24e3c6592be0d637c094b9830674037d05535000
Alex <almartin86@yahoo.fr>
2020-03-23 13:27:10 +0100

    decoupage du dashbord et de la table des produit

---
+++ views/table.php
@@ -0,0 +1,69 @@
+<div class="bg-light col-sm-8 col-md-8 col-lg-9 col-xl-9">
+    <!--CORP-->
+    <table class="table table-hover table-secondary container-fluid">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Prenom</th>
+                <th scope="col">Mail</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Martin</td>
+                <td>Alexandre</td>
+                <td>alexandremartin@gmail.fr</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Murashka</td>
+                <td>Olga</td>
+                <td>murashka.olga@gmail.fr</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ostalier</td>
+                <td>Laurent</td>
+                <td>ostalier.laurent@gmail.fr</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ostalier</td>
+                <td>Laurent</td>
+                <td>ostalier.laurent@gmail.fr</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ostalier</td>
+                <td>Laurent</td>
+                <td>ostalier.laurent@gmail.fr</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ostalier</td>
+                <td>Laurent</td>
+                <td>ostalier.laurent@gmail.fr</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ostalier</td>
+                <td>Laurent</td>
+                <td>ostalier.laurent@gmail.fr</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ostalier</td>
+                <td>Laurent</td>
+                <td>ostalier.laurent@gmail.fr</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ostalier</td>
+                <td>Laurent</td>
+                <td>ostalier.laurent@gmail.fr</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
